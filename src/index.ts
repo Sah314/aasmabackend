c5b41baf7c5c0a3ba1b37342fdc2134a77fa5bbc
sah_314 <khadayatesahil@gmail.com>
2023-12-14 12:41:58 +0530

    feat: added endpoint to fetch top repos e.g 10,50,100

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import {Request,Response} from "express";
 import express from "express";
 import axios from "axios";
-import dta from "./utils/fetchdata";
+import fetchData from "./utils/fetchdata";
 const app  =express();
 console.log("HEllo");
 
-const githubdata = dta;
+
 const PORT = 3000
 
 app.get('/v1/health',(req:Request,res:Response)=>{
-    console.log(githubdata);
     res.json("Api is healthy");
 })
 
+app.get('/v1/top/:num',async(req:Request,res:Response)=>{
+    const {num}= req.params;
+    console.log(num);
+    try {
+    const data = await fetchData(Number(num),1);
+    console.log(data.items.length);
+        if(data){
+           return res.status(202).json({data:data});
+        }
+        else{
+            throw res.status(404)
+        }
+    } catch (error) {
+        console.error(error)
+    }
 
+})
 app.listen(PORT,()=>{
 console.log(`App running on 3000`);
 })
--- src/utils/fetchdata.ts
+++ src/utils/fetchdata.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const url = 'https://api.github.com/search/repositories?q=created:2019-01-10&sort=stars&order=desc';
 
-async function fetchData(count:Number,pageno:Number) {
+async function fetchData(count:number,pageno:number) {
   try {
     const response = await axios.get(`${url}&per_page=${count}&page=${pageno}`);
     const data = response.data;
-    console.log('Fetched data:', data);
+    //console.log('Fetched data:', data);
     return data;
   } catch (error:any) {
     console.error('Error fetching data:', error.message);
